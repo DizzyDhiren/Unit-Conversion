930a0f873f81a154f66792e618e244ca02ddaf15
DizzyDhiren <dhiren007patel@gmail.com>
2026-06-16 17:02:03 +0100

    fix: JS completed with correct rations and varibles. Prints to HTML and functionality works

--- index.js
+++ index.js
@@ -5,27 +5,37 @@ const volumeOutput = document.getElementById("volume-output")
 const massOutput = document.getElementById("mass-output")
 
 const lengthRatio = 3.281;
-const volumeRatio = 10;
-const massRatio = 10;
+const volumeRatio = 0.264;
+const massRatio =  2.204;
 
 convertBtn.addEventListener("click" , function(){
     //functions for converts go here
     const num = Number(inputEl.value)
     findLength(num)
+    findVolume(num)
+    findMass(num)
 })
 
 function findLength(num){
     const metersToFeet = (num * lengthRatio).toFixed(3)
     const feetToMeters = (num / lengthRatio).toFixed(3)
-     console.log(num)
+     
  lengthOutput.innerHTML = 
- `${inputEl.value} Meters = ${metersToFeet} feet | ${inputEl.value} feet = ${feetToMeters} meters`
+ `${num} Meters = ${metersToFeet} Feet | ${num} Feet = ${feetToMeters} Meters`
 }
 
 function findVolume(num){
-
+    const litersToGallons= (num * volumeRatio).toFixed(3)
+    const gallonsToLiters = (num / volumeRatio).toFixed(3)
+     
+ volumeOutput.innerHTML = 
+ `${num} Liters = ${litersToGallons} Gallons | ${num} Gallons = ${gallonsToLiters} Liters`
 }
 
 function findMass(num){
-
+    const kilosToPounds = (num * massRatio).toFixed(3)
+    const poundsToKilos = (num / massRatio).toFixed(3)
+     
+ massOutput.innerHTML = 
+ `${num} Kilos = ${kilosToPounds} Pounds | ${num} Pounds = ${poundsToKilos} Kilos`
 }
